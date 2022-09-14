64bc78d4bf537e54b425428a19c9f82ac2bb4a6f
tril <healingdrawing@gmail.com>
2022-09-14 12:35:32 +0300

    code cleaning

--- src/auth.c
+++ src/auth.c
@@ -37,29 +37,19 @@ void loginMenu(char a[50], char pass[50])
 
 const char *getPassword(struct User u)
 {
-    // printf("inside getpasswordcode");
     FILE *fp;
     struct User userChecker;
-    // printf("before if fp open auth.c line 42\n");
     if ((fp = fopen("./data/users.txt", "r")) == NULL)
     {
         printf("Error! opening file");
         exit(1);
     }
-    // printf("before while fscanf auth.c line 47\n");
     while (fscanf(fp, "%i %s %s", &userChecker.id, userChecker.name, userChecker.password) != EOF)
     {
-        // printf("before strcomp auth.c line 50\n");
-        // printf("\nuserChecker.name ");
-        // printf("%s",userChecker.name);
-        // printf("\nuserChecker.password ");
-        // printf("%s",userChecker.password);
-        // printf("\n");
         if (strcmp(userChecker.name, u.name) == 0)
         {
             fclose(fp);
             char *buff = userChecker.password;
-            // printf("%s", buff);
             return buff;
         }
     }
@@ -143,5 +133,8 @@ registerAgain:
         printf("Passwords was not identical");
         goto registerAgain;
     }
+
+    // new user data is ok, write to database
+
 
 };
